bde4dd507a8cdff1afe122bef0d6b32aedf49707
Tazzz19 <hamzatrahman92@gmail.com>
2024-03-14 16:54:30 +0100

    done

---
+++ main/routes/personRoutes.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const router = express.Router();
+const personController = require('../controllers/personControllers');
+
+router.get('/', personController.getAllPersons);
+router.post('/createPerson', personController.createPerson);
+router.post('/generateRandomUsers', personController.generateRandomUsers);
+router.delete('/deletePerson/:id', personController.deletePersonById);
+router.put('/updatePerson/:id', personController.updatePersonById);
+
+
+module.exports = router;
